757f6653bd4ab7f57aacfcfaa5413bcad45be518
RamMaths <ramses.hdz30@gmail.com>
2025-10-03 21:35:11 -0600

    Add elixirls configuration to lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -1,6 +1,6 @@
 require("nvchad.configs.lspconfig").defaults()
 
-local servers = { "html", "cssls", "jedi_language_server", "ts_ls", "angularls", "bashls", "dockerls", "markdown_oxide", "terraformls", "rust_analyzer" }
+local servers = { "html", "cssls", "jedi_language_server", "ts_ls", "angularls", "bashls", "dockerls", "markdown_oxide", "terraformls", "rust_analyzer", "elixirls" }
 vim.lsp.enable(servers)
 
 -- read :h vim.lsp.config for changing options of lsp servers 
